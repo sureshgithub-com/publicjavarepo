0c5d8e9ba2e21ffe639625843a8c129c4725f10b
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2025-06-19 13:10:15 +0530

    Added: My Java Projects

---
+++ IBC_Java_Projects/Ibc.java
@@ -0,0 +1,21 @@
+public class Ibc {
+
+    class First {
+
+        static int Add(int a, int b) {
+            return a + b;
+        }
+
+
+    public static void main(String args[]) {
+        System.out.println("Learn Java Classes and Objects");
+        First obj = new First();
+        int c = First.Add(5, 555);
+
+        System.out.println("Add : " + c);
+
+
+    }
+}
+
+}

e83aaf37af1141a317ea51903bd34238a4711d33
NCoder-Java <nikitabalajibacha@gmail.com>
2025-06-20 23:31:42 +0530

    Data Types Range - min & max values

---
+++ DataTypesRange.java
@@ -0,0 +1,20 @@
+//  The range (min & max values) of primitive data types and Math.PI 
+
+public class DataTypesRange
+{
+    public static void main(String... args) {
+        System.out.println(Integer.MAX_VALUE); //  2147483647
+        System.out.println(Integer.MIN_VALUE); // -2147483648
+        System.out.println(Long.MIN_VALUE);    // -9223372036854775808
+        System.out.println(Long.MAX_VALUE);    //  9223372036854775807
+        System.out.println(Short.MAX_VALUE);   //  32767
+        System.out.println(Short.MIN_VALUE);   // -32768
+        System.out.println(Byte.MIN_VALUE);    // -128
+        System.out.println(Byte.MAX_VALUE);    //  127
+        System.out.println(Float.MIN_VALUE);   // 1.4E-45 (smallest positive value)
+        System.out.println(Float.MAX_VALUE);   // 3.4028235E38
+        System.out.println(Character.MIN_VALUE); // '\u0000' (null char = 0)
+        System.out.println(Character.MAX_VALUE); // '\uffff' (65535)
+        System.out.println(Math.PI);            // 3.141592653589793
+    }
+}

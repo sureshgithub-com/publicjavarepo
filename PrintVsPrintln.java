a4abd95bbb05d0c818f88c4907c7a79439c6e209
SujataSaundale07 <s.saundale16@gmail.com>
2025-06-20 22:33:49 -0700

    Sujata - PrintVsPrintln

---
+++ PrintVsPrintln.java
@@ -0,0 +1,10 @@
+public class PrintVsPrintln {
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.out.print(" IBC");
+        System.out.println("!");
+        System.out.println("this prints at new line");
+    }
+}
+//output--Hello IBC!
+//        this prints at new line
